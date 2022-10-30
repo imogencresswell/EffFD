520a7d39d95a7def34a0db1f40174e2524737af0
Isaiah Tristan <Isaiah.Tristan@colorado.edu>
2022-10-30 12:23:39 -0600

    added base for searchign for stars based on temperature or spectral type, added comments for functions needed to continue that, added line to remove folders that had no data returned, minor formatting changes

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ Usage:
     main.py <config> [options]
 
 Options:
-    --star_names=<star>            Names of stars  [default: AU Mic]
-    --root_dir=<dir>               Root directory for outputs [default: ./]
-    --savgov_iterations=int        Iterations for lc.flatten [default: 9]
-    --savgov_sigma_cutoff=int      Sigma cutoff for lc.flatten [default: 3]
+    --root_dir=<dir>               Root directory for outputs  [default: ./]
+
+    --star_names=<star>            Names of stars
+    --spectral_type=<type>         Spectral type to search for stars
+    --teff_low=<temp>              Lower limit of Teff to search [default: 2000]
+    --teff_high=<temp>             Lower limit of Teff to search [default: 3500]
+
+    --savgov_iterations=<iter>     Iterations for lc.flatten  [default: 9]
+    --savgov_sigma_cutoff=<sig>    Sigma cutoff for lc.flatten  [default: 3]
 
 """
 import numpy as np
@@ -20,6 +25,23 @@ from configparser import ConfigParser
 from pathlib import Path
 import csv
 import os
+
+
+def get_spectral_temp(classification):
+    if classification=='M':
+        return 2000, 3500  # low temp limit, high temp limit, in K
+    elif classification=='K':
+        return 3500, 5000
+    elif classification=='G':
+        return 5000, 6000
+    elif classification=='F':
+        return 6000, 7500
+    elif classification=='A':
+        return 7500, 10000
+    elif classification=='B':
+        return 10000, 30000
+    elif classification=='O':
+        return 30000, 60000
 
 
 def save_raw_lc(object, save_path, filter_iter, filter_sig):
@@ -54,6 +76,7 @@ def main():
                     if v == 'true':
                         v = True
                     args[k] = v
+    #print(args)
 
     # PROBEMS/QUERIES:
 
@@ -65,31 +88,49 @@ def main():
     if args['--star_names'] is not None:
         star_names = str(args['--star_names'])
         star_names_list = list(map(str.strip, star_names.split(',')))
+    else:
+        # Uses temperature-range star search if no names/spectral_type given
+        teff_low = int(args['--teff_low'])
+        teff_high = int(args['--teff_high'])
 
-        print('\n###############################')
-        print('Starting individual star search')
-        print('###############################\n')
+        if args['--spectral_type'] is not None:
+            spectral_type = str(args['--spectral_type'])
 
-        for star in star_names_list:
-            star_path = os.path.join(str(args['--root_dir']),
-                                     star.replace(' ', '_'))
+            # Error catching for unsupported types. Stop run if unsupported.
+            if spectral_type in 'L,T,Y'.split(','):
+                raise ValueError('Brown dwarfs are not yet supported.')
+            elif spectral_type not in 'O,B,A,F,G,K,M'.split(','):
+                raise ValueError('Please use spectral type in OBAFGKM.')
 
-            try:  # Keeps program running if folders already exist
-                os.mkdir(star_path)
-            except FileExistsError:
-                print('A folder for {} already exists.'.format(star))
+            else:
+                teff_low, teff_high = get_spectral_temp(spectral_type)
 
-            try:  # Keeps program running if a star name is not searchable
-                save_raw_lc(star,
-                            star_path,
-                            int(args['--savgov_iterations']),
-                            int(args['--savgov_sigma_cutoff']))
-            except FileNotFoundError:
-                print('No search results found for {}.'.format(star))
-                # If we add other functions under this and they all fail
-                # we could have an os.rmdir here.
+        # -- Astroquery search for list of stars by temperature
+        star_names_list = []
 
-            print('Operations for {} finished.\n'.format(star))
+
+    print('\n###############################')
+    print('Starting individual star search')
+    print('###############################\n')
+    for star in star_names_list:
+        star_path = os.path.join(str(args['--root_dir']),
+                                 star.replace(' ', '_'))
+
+        try:  # Keeps program running if folders already exist
+            os.mkdir(star_path)
+        except FileExistsError:
+            print('A folder for {} already exists.'.format(star))
+
+        try:  # Keeps program running if a star name is not searchable
+            save_raw_lc(star,
+                        star_path,
+                        int(args['--savgov_iterations']),
+                        int(args['--savgov_sigma_cutoff']))
+        except FileNotFoundError:
+            print('No search results found for {}.'.format(star))
+            os.rmdir(star_path)
+
+        print('Operations for {} finished.\n'.format(star))
 
 
 if __name__ == '__main__':
